e3cd4efda07b58a8e63d70d5067d41a9289df88a
@fredmuli <fnfredmlie@gmail.com>
2023-11-14 15:24:05 -0800

    Betty style and debugging

--- nprintf.c
+++ nprintf.c
@@ -1,9 +1,5 @@
 #include "main.h"
 
-#include <string.h>
-#include <stdarg.h>
-#include <unistd.h>
-
 /**
  * _printf - alt functions to printf
  *
@@ -14,54 +10,51 @@
 int _printf(const char *format, ...)
 {
 	int new_charpr = 0;
-
 	va_list arlist;
 
 	if (format == NULL)
-
-	return (-1);
+		return (-1);
 
 	va_start(arlist, format);
-		while (*format)
+	while (*format)
+	{
+		if (*format != '%')
 		{
-			if (*format != '%')
+			write(1, format, 1);
+			new_charpr++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+
+			if (*format == '%')
 			{
 				write(1, format, 1);
 				new_charpr++;
 			}
-			else
+			else if (*format == 'c')
 			{
-				format++;
-				if (*format == '\0')
-				{
-					break;
-				}
-				if (*format == '%')
-				{
-					write(1, format, 1);
-					new_charpr++;
-				}
-				else if (*format == 'c')
-				{
-					char c = va_arg(arlist, int);
-
-					write(1, &c, 1);
-					new_charpr++;
-				}
-				else if (*format == 's')
-				{
-					char *str = va_arg(arlist, char *);
-					int strlen = 0;
-
-					do {
-						write(1, str, strlen);
-					}
-					while (str[strlen] != '\0');
-						strlen++;
-				}
+				char c = va_arg(arlist, int);
+				write(1, &c, 1);
+				new_charpr++;
 			}
-			format++;
+			else if (*format == 's')
+			{
+				char *str = va_arg(arlist, char*);
+				int strlen = 0;
+
+				while (str[strlen] != '\0')
+					strlen++;
+
+				write(1, str, strlen);
+				new_charpr += strlen;
+			}
 		}
+
+		format++;
+	}
 	va_end(arlist);
 	return (new_charpr);
 }
